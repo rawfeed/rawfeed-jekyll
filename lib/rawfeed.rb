f7aa23c5b7dd5e6721ca2c373bca1000ff6d27f7
William C. Canin <william.costa.canin@gmail.com>
2025-10-14 00:44:08 -0300

    Importing datelang and reading_time

--- lib/rawfeed.rb
+++ lib/rawfeed.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+# author: William C. Canin
+
 require_relative "rawfeed/version"
 require_relative "rawfeed/draft"
 require_relative "rawfeed/post"
 require_relative "rawfeed/page"
 require_relative "rawfeed/resume"
 require_relative "rawfeed/layout"
+require "rawfeed/datelang"
+require "rawfeed/reading_time"
