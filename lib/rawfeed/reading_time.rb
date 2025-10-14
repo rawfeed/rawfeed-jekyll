38174e7801d93f5e6b276722d36f1342722fba51
William C. Canin <william.costa.canin@gmail.com>
2025-10-14 00:44:55 -0300

    Adding reading_time plugin

---
+++ lib/rawfeed/reading_time.rb
@@ -0,0 +1,17 @@
+require "jekyll"
+
+module Jekyll
+  module ReadingTimeFilter
+    def reading_time(input)
+      site_config = Jekyll.sites.first.config rescue {}
+      words_per_minute = site_config&.dig("reading_time", "words_per_minute") || 180
+      message = site_config&.dig("reading_time", "message") || ["Read this post in", "Read this post in less than"]
+      minutes_text = site_config&.dig("reading_time", "minutes_label") || ["minute", "minutes"]
+      words = input.split.size;
+      minutes = ( words / words_per_minute ).floor
+      minutes_label = minutes == 1 ? "#{minutes_text[0]}" : "#{minutes_text[1]}"
+      minutes > 0 ? "#{message[0]} #{minutes} #{minutes_label}" : "#{message[1]} 1 #{minutes_text[0]}"
+    end
+  end
+end
+Liquid::Template.register_filter(Jekyll::ReadingTimeFilter)
