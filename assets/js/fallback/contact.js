b7e70758412db398734d055d7871418fde831aa2
William C. Canin <william.costa.canin@gmail.com>
2026-03-19 01:44:30 -0300

    fix: changing CORS to text/plain instead of json

--- assets/js/fallback/contact.js
+++ assets/js/fallback/contact.js
@@ -68,7 +68,9 @@ document.addEventListener("DOMContentLoaded", () => {
       try {
         const response = await fetch(endpoint, {
           method: "POST",
-          redirect: "follow",
+          headers: {
+            "Content-Type": "text/plain;charset=utf-8"
+          },
           body: JSON.stringify(data)
         });
 
