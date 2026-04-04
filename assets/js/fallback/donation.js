52d9bfefcc625de6bc932aa2f6ce5df06be7edc4
William C. Canin <william.costa.canin@gmail.com>
2026-04-04 02:25:52 -0300

    feature: add donation script

---
+++ assets/js/fallback/donation.js
@@ -0,0 +1,30 @@
+---
+---
+
+{%- include layout/data.liquid -%}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const donation = document.getElementById("donation");
+  if (!donation) return;
+
+  document.querySelectorAll(".donation__btn--copy").forEach(btn => {
+    btn.addEventListener("click", function () {
+      navigator.clipboard.writeText(this.dataset.copy).then(() => {
+        this.innerText = "{{ donation_.copy_button.clicked }}";
+        setTimeout(() => this.innerText = "{{ donation_.copy_button.text }}", 2000);
+      });
+    });
+  });
+
+  // QR MODAL
+  const qrModal = document.getElementById('qrModal');
+  const qrModalImg = document.getElementById('qrModalImg');
+
+  document.querySelectorAll(".clickable-qr").forEach(img => {
+    img.addEventListener("click", function () {
+      qrModalImg.src = this.dataset.qr;
+    });
+  });
+
+});
