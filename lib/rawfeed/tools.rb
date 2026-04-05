c5bd496730d5da713cd1325011af7fe1634e2ebf
William C. Canin <william.costa.canin@gmail.com>
2026-04-05 13:55:21 -0300

    feature: add code to create template and node packages install

--- lib/rawfeed/tools.rb
+++ lib/rawfeed/tools.rb
@@ -2,6 +2,28 @@ require "open3"
 
 module Rawfeed
   module Tools
+    TEMPLATE_DIR = File.expand_path("../../template", __dir__)
+    NODE_TOOLS_DIR = File.expand_path("../../tools", __dir__)
+
+    def self.init(target_dir = Dir.pwd)
+      puts "[*] Initializing site in #{target_dir}..."
+      FileUtils.mkdir_p(target_dir)
+      FileUtils.cp_r(Dir[File.join(TEMPLATE_DIR, "*")], target_dir)
+      puts "[*] Site template copied."
+    end
+
+    def self.install
+      # executa npm install na pasta tools interna
+      Dir.chdir(NODE_TOOLS_DIR) do
+        if !File.exist?("package.json")
+          puts "[!] package.json not found in tools!".red
+          return
+        end
+        puts "[*] Installing Node dependencies..."
+        system("npm install")
+      end
+    end
+
     def self.minify
       gem_dir = Gem::Specification.find_by_name("rawfeed").gem_dir
       script  = File.join(gem_dir, "tools", "scripts", "minify.mjs")
