63e238d6943aeba9be05f730dd46d0a92dde2742
William C. Canin <william.costa.canin@gmail.com>
2026-04-07 22:15:43 -0300

    fix: add rubyzip dependency

--- lib/rawfeed/command/installer.rb
+++ lib/rawfeed/command/installer.rb
@@ -86,7 +86,10 @@ module Rawfeed
         # frozen_string_literal: true
         source "https://rubygems.org"
 
-        ## Do not modify this block unless you know what you are doing.
+        # Dependencies
+        gem "rubyzip", "~> 2.3"
+
+        ## [ rawfeed ] Do not modify this block unless you know what you are doing.
         if ENV["RAWFEED_DEV_PATH"] && File.directory?(ENV["RAWFEED_DEV_PATH"])
           gem "rawfeed", path: ENV["RAWFEED_DEV_PATH"]
         else
