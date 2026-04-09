5bb282a66536ff10fd51124f77e28b599bd5bfba
William C. Canin <william.costa.canin@gmail.com>
2026-04-09 10:02:29 -0300

    updating: replacing new values ​​after updating _data

--- lib/rawfeed/plugin/reading_time.rb
+++ lib/rawfeed/plugin/reading_time.rb
@@ -4,10 +4,11 @@ module Rawfeed
   module ReadingTimeFilter
     def reading_time(input)
       site = Jekyll.sites.first rescue nil
-      options_data = site&.data&.dig("options") rescue {}
-      words_per_minute = options_data&.dig("reading_time", "words_per_minute") || 180
-      message = options_data&.dig("reading_time", "message") || ["Read this post in", "Read this post in less than"]
-      minutes_text = options_data&.dig("reading_time", "minutes_label") || ["minute", "minutes"]
+      # blog_data = site.data['screen', 'blog'] rescue {}
+      blog_data = site&.data&.dig("screen", "blog") rescue {}
+      words_per_minute = blog_data&.dig("post", "reading_time", "words_per_minute") || 180
+      message = blog_data&.dig("post", "reading_time", "message") || ["Read this post in", "Read this post in less than"]
+      minutes_text = blog_data&.dig("post", "reading_time", "minutes_label") || ["minute", "minutes"]
       words = input.split.size;
       minutes = ( words / words_per_minute ).floor
       minutes_label = minutes == 1 ? "#{minutes_text[0]}" : "#{minutes_text[1]}"
